17a14ca9748569c8f07b8ec4b48da9939ffbe94f
vitordsb <vitordsb2019@gmail.com>
2026-05-27 12:20:35 -0300

    feat: adiciona meta pixel

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from "react";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { MetaPixel } from "@/components/MetaPixel";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Suspense fallback={null}>
+        <MetaPixel />
+      </Suspense>
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
---
+++ src/components/MetaPixel.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const META_PIXEL_ID = "1849119642473487";
+
+declare global {
+  interface Window {
+    fbq?: (...args: unknown[]) => void;
+    _fbq?: (...args: unknown[]) => void;
+  }
+}
+
+export function MetaPixel() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    if (!didMount.current) {
+      didMount.current = true;
+      return;
+    }
+
+    window.fbq?.("track", "PageView");
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
+}
